91e89aab4d16f15e22e5104ec6b21f4f10d180e6
mooshaa <37233264+mooshaa@users.noreply.github.com>
2021-04-08 21:01:59 -0700

    Update config.py

--- bots/config.py
+++ bots/config.py
@@ -18,8 +18,8 @@ def api():
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
     
-    auth = tweepy.OAuthHandler("gZV93qkxK6lvrxBOguoyx5arb","zbVxx1qSLoS8Y517DYTmYFuUUZtBtZbZviOWXRlXlA9Lc1qFVJ")
-    auth.set_access_token("929957003929141248-a67kr90z44xBVjJiZTHtTgkEkC8kXbk","OPiPSWPNfWDNyHy7zPVYr6FUkxNO8SUK22n9jfFjfFqQb")
+    auth = tweepy.OAuthHandler("","")
+    auth.set_access_token("","")
     api = tweepy.API(auth, wait_on_rate_limit=(True),wait_on_rate_limit_notify=(True))
     try:
         api.verify_credentials()
